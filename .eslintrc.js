b3e7a003b88f8944919974267b89cdd75637c6f4
percy507 <percymong@163.com>
2023-10-20 00:11:16 +0800

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['@react-native'],
   plugins: ['simple-import-sort'],
   rules: {
+    curly: 0,
     'no-unused-vars': 0,
     'no-undef': 0,
     'simple-import-sort/imports': 2,
@@ -10,8 +11,10 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/no-unstable-nested-components': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-shadow': 0,
     '@typescript-eslint/no-unused-vars': [2],
     '@typescript-eslint/consistent-type-imports': [2],
     '@typescript-eslint/no-duplicate-imports': 2,
+    'react-native/no-inline-styles': 2, // 不要写行内样式，尽量将样式封装至组件或全局样式token中
   },
 };
