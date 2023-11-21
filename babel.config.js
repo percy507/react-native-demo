07fbc20957de5eb96ebd4f8f09958159a191b8e5
percy507 <percymong@163.com>
2023-11-21 18:04:53 +0800

    feat: 配置metro和babel，从而支持以字符串的形式行内引入html文件

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,12 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      [
+        'inline-import',
+        {
+          extensions: ['.html'],
+        },
+      ],
       './plugins/babel-plugin-rn-wrap-px2dp/index.js',
       'react-native-reanimated/plugin',
     ],
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { mergeConfig } = require('@react-native/metro-config');
+const metroDefault = require('metro-config/src/defaults/defaults.js');
 
 /**
  * Metro configuration
@@ -10,6 +11,7 @@ const { mergeConfig } = require('@react-native/metro-config');
 const config = {
   resolver: {
     unstable_enableSymlinks: true,
+    assetExts: metroDefault.assetExts.concat(['html']),
   },
 };
 
